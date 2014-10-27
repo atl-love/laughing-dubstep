974cec4d354e6f34a3af7aa53a3f94eed826009b
Sai Karra <saikarra@gmail.com>
2014-10-26 21:25:01 -0700

    test project for netty/protobuf

---
+++ p1test/p1lifeforce/src/main/java/edu/sjsu/cmpe275/p1lifeforce/LifeForce.java
@@ -0,0 +1,13 @@
+package edu.sjsu.cmpe275.p1lifeforce;
+
+/**
+ * Hello world!
+ *
+ */
+public class LifeForce 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
