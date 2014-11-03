653a3009444f43f12aea76ce29754834953d43cd
Ryan Vo <ryan_vo@symantec.com>
2014-11-02 21:18:50 -0800

    Added code to close channel when done sending info. Fixed a bug with checking for empty clusterName

--- p1test/nettyDNS/src/dns/DNSServerHandler.java
+++ p1test/nettyDNS/src/dns/DNSServerHandler.java
@@ -3,6 +3,8 @@ package dns;
 import java.util.HashMap;
 import java.util.Set;
 
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import dns.DNSProtocol.MasterNode;
@@ -14,33 +16,51 @@ public class DNSServerHandler extends SimpleChannelInboundHandler<DNSRequest> {
     static final HashMap<String, MasterNode> MASTERNODES = new HashMap<>();
 
     @Override
-    public void channelRead0(ChannelHandlerContext ctx, DNSRequest request) {
+    public void channelRead0(final ChannelHandlerContext ctx, DNSRequest request) {
+    	
+    	DNSResponse response;
+    	
+    	// Handle request.
+    	System.out.println("Received:\n" + request.toString());
+    	
     	if (request.getAction() == DNSRequest.Action.GET){
-    		handleGET(ctx, request);
+    		response = handleGET(request);
     	}
     	else if (request.getAction() == DNSRequest.Action.PUT) {
-    		handlePUT(ctx, request);
+    		response = handlePUT(request);
     	}
-    	else {
+    	else { // Handle error.
     		DNSResponse.Builder rb = DNSResponse.newBuilder();
     		rb.setStatus(DNSResponse.Status.ERROR);
     		rb.setErrorMessage("Not a valid Action");
-    		ctx.write(rb.build());
+    		response = rb.build();
     	}
+    	
+    	// Respond.
+    	System.out.println("Sending:\n" + response.toString());
+    	final ChannelFuture f = ctx.writeAndFlush(response);
+    	f.addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture future) {
+                assert f == future;
+                System.out.println("Closing channel.");
+                ctx.close();
+            }
+        });
     }
     
-    private void handleGET(ChannelHandlerContext ctx, DNSRequest request){
+    private DNSResponse handleGET(DNSRequest request){
     	
     	String clusterName = request.getClusterName();
     	
-    	if (clusterName == null){ // asking for all clusters
+    	if (clusterName.equals("")){ // asking for all clusters
     		DNSResponse.Builder rb = DNSResponse.newBuilder();
             rb.setStatus(DNSResponse.Status.OK);
             Set<String> clusterNames = MASTERNODES.keySet();
             for (String name : clusterNames){
             	rb.addMasterNode(MASTERNODES.get(name));
             }
-            ctx.write(rb.build());
+            return rb.build();
     	} 
     	else { // asking for specific cluster
     		MasterNode m = MASTERNODES.get(clusterName);
@@ -48,29 +68,27 @@ public class DNSServerHandler extends SimpleChannelInboundHandler<DNSRequest> {
 	    		DNSResponse.Builder rb = DNSResponse.newBuilder();
 	            rb.setStatus(DNSResponse.Status.OK);
 	            rb.addMasterNode(m);
-	            ctx.write(rb.build());
+	            return rb.build();
 	    	}
 	    	else { // not found
 	    		DNSResponse.Builder rb = DNSResponse.newBuilder();
 	    		rb.setStatus(DNSResponse.Status.ERROR);
 	    		rb.setErrorMessage("Cluster not found");
-	    		ctx.write(rb.build());
+	    		return rb.build();
 	    	}
     	}
     }
     
-    private void handlePUT(ChannelHandlerContext ctx, DNSRequest request){
+    private DNSResponse handlePUT(DNSRequest request){
+    	// Insert first.
     	MasterNode m = request.getMasterNode();
     	MASTERNODES.put(m.getClusterName(), m);
+    	
+    	// Respond.
     	DNSResponse.Builder rb = DNSResponse.newBuilder();
         rb.setStatus(DNSResponse.Status.OK);
         rb.addMasterNode(m);
-        ctx.write(rb.build());
-    }
-
-    @Override
-    public void channelReadComplete(ChannelHandlerContext ctx) {
-        ctx.flush();
+        return rb.build();
     }
 
     @Override
